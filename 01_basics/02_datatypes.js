fbf09ebf2673208cd57ecb9ef517b67250f3c16c
madhusarania <110324250+madhusarania@users.noreply.github.com>
2024-04-24 14:59:42 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -21,7 +21,16 @@ let city = "Assam";
 4> boolean = true/false
 5> null = stnadlone value
 6> undefined
+7> symbol = unique
+
+8> object
 */
+
+console.log(typeof age);
+console.log(typeof "Madhu")
+console.log(typeof null) // so it is a type of object
+console.log(typeof undefined) // and this undefine is a type of undefined
+
 
 
 
