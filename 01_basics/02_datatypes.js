30e950acffda5c9bfb1525f5b6afc0cc78500653
madhusarania <110324250+madhusarania@users.noreply.github.com>
2024-04-24 14:59:17 +0000

    variables

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version or JS
+
+// alert(3+3) we are  using nodejs not browser so cant able to run in nodejs
+
+console.log( 3 + 3); 
+
+// code readability should be high
+
+console.log("Madhu")
+
+let name = "Madhu"
+let age = 25
+let isLoggedIn = true
+let state; //undefined
+let city = "Assam";
+
+/* Datatypes 
+1> number = 2 to power 53
+2> bigInt
+3> string = ""
+4> boolean = true/false
+5> null = stnadlone value
+6> undefined
+*/
+
+
+
