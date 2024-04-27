ec18050bf36efc42db2456c94155c20ab3397fd5
madhusarania <110324250+madhusarania@users.noreply.github.com>
2024-04-27 11:31:32 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "Madhu"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1 ; false => 0
+
+let loggedIn = "Madhu"
+let booleanLoggedIn = Boolean(loggedIn)
+
+// console.log(booleanLoggedIn);
+// console.log(typeof booleanLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false ; "Madhu" => true
+
+
+let someScore = "33abc"
+
+let stringInConvert = String(someScore)
+
+console.log(stringInConvert);
+console.log(typeof stringInConvert);
