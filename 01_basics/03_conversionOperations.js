6f5250fe37e147059327654b0cc5aa5a6046533c
madhusarania <110324250+madhusarania@users.noreply.github.com>
2024-05-07 02:12:12 +0000

    comparison datatype

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,5 +26,30 @@ let someScore = "33abc"
 
 let stringInConvert = String(someScore)
 
-console.log(stringInConvert);
-console.log(typeof stringInConvert);
+// console.log(stringInConvert);
+// console.log(typeof stringInConvert);
+
+// ******************************Operations*****************************************
+
+let value = -3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " madhu"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log(true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
